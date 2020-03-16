097f0fe5d14e98443cddec7d9ef3786ba0deb4a6
Darciechapman <darciechapman96@gmail.com>
2020-03-16 11:18:26 +0800

    local storage works !! yay

--- script.js
+++ script.js
@@ -9,8 +9,9 @@ var currenthour = moment().format('LT')
 document.getElementById("currentDay").innerHTML = (dayDate + "/" + monthDate + "/" + yearDate);
 
 //parse momment
+//set a value
+var displayTime = ["9:00am", "10:00am", "11:00am", "12:00pm", "1:00pm", "2:00pm", "3:00pm", "4:00pm", "5:00pm"]
 
-var displayTime = ["9", "10", "11", "12", "13", "14", "15", "16", "17"]
 
 var time = ""
 
@@ -19,7 +20,8 @@ displayTime.forEach(function(element) {
     
     var row = $("<div>").addClass("row"); 
     var timeBlock = $("<div>").addClass("col-sm-1 time-block");
-    var input = $("<input>").addClass("col-sm-10");
+    var input = $('<input type="text">').addClass("col-sm-9");
+    //var checkBtn = $("<button>").addClass("col-sm-1");
     var saveBtn = $("<button>").addClass("col-sm-1 saveBtn");
 
     if (displayTime > currenthour) {
@@ -31,12 +33,15 @@ displayTime.forEach(function(element) {
     }
 
     timeBlock.text(element);
+    input.attr('id', element); 
+    saveBtn.attr('id', element);
     //timeBlock.innerHTML = JSON.stringify(element)
     //console.log(element.toString())
     
 
     row.append(timeBlock);
     row.append(input);
+    //row.append(checkBtn);
     row.append(saveBtn);
     
     //let hour = JSON.stringify(element)
@@ -47,14 +52,15 @@ displayTime.forEach(function(element) {
 
 });
 
-$("button").on("click", function() {
-    var inputText = $("input").text
-    if (inputText === "") {
-        alert("error", "Email cannot be blank");
-    } else {
-        storeToDo();
-    }
-});
+//target event
+//$("button").on("click", function() {
+//    var inputText = $("input").text
+ //   if (inputText === "") {
+ //       alert("error", "Email cannot be blank");
+ //   } else {
+ //       storeToDo();
+  //  }
+//});
 
 
 
@@ -63,12 +69,34 @@ $("button").on("click", function() {
 //}
 
 
-function storeToDo() {
-   var inputText = $("input").text
-   JSON.parse(localStorage.getItem("todoData"));
-   localStorage.setItem("todoData", JSON.stringify(inputText));
+$("button").on("click", function() {
+    $('input[type="text"]').blur(function() {
+        if(!$(this).val()) {
+            alert("empty")
+        } else {
+            alert("not empty")
+            storeToDo();
+        }
+    })
+})
 
-}
+
+function storeToDo() {
+        if (typeof (window.localStorage) != "undefined") {
+            //set the value to the text fields
+            $("input[type=text]").val(function () {
+                return localStorage.getItem(this.id);
+            });
+            $("input[type=text]").on("change", function () {
+                localStorage.setItem(this.id, $(this).val());
+            });
+        }
+    };
+
+
+
+//add a check button
+//<i class="fas fa-check"></i>
 
 
 
